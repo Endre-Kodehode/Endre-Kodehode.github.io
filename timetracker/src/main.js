7db18dd038753e741dd74a38dc17853f4f47a4b1
Endre-Kodehode <93523916+Endre-Kodehode@users.noreply.github.com>
2022-01-25 12:20:08 +0100

    a
    a

--- timetracker/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-createApp(App).mount("#app");
-
-// hele appen injectes inn i en enkelt div i index.html,
-// som kun inneholder en tom div.
